63b25f155580e4fd4710bb683ed42148e830b756
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 20:05:04 +0300

    Implement load function to redirect to the page url

--- src/test/java/com/qacart/tasky/pages/RegisterPage.java
+++ src/test/java/com/qacart/tasky/pages/RegisterPage.java
@@ -1,8 +1,11 @@
 package com.qacart.tasky.pages;
 
+import static com.qacart.tasky.configs.ConfigFactory.getConfig;
+import static com.qacart.tasky.driver.managers.DriverManager.getDriver;
+
 public class RegisterPage implements BasePage {
     @Override
     public void loadPage() {
-        
+        getDriver().get(getConfig().pageBaseURL() + "/register");
     }
 }
--- src/test/java/com/qacart/tasky/pages/SubscriptionPage.java
+++ src/test/java/com/qacart/tasky/pages/SubscriptionPage.java
@@ -1,8 +1,11 @@
 package com.qacart.tasky.pages;
 
+import static com.qacart.tasky.configs.ConfigFactory.getConfig;
+import static com.qacart.tasky.driver.managers.DriverManager.getDriver;
+
 public class SubscriptionPage implements BasePage {
     @Override
     public void loadPage() {
-
+        getDriver().get(getConfig().pageBaseURL() + "/dashboard/subscription");
     }
 }
