e365657de95ee6826847263f4757956c56047184
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 23:12:02 +0300

    Implement maximum todos alert message test case for regular user

--- src/test/java/com/qacart/tasky/testcases/MaximumTodosAlertRegularUserTest.java
+++ src/test/java/com/qacart/tasky/testcases/MaximumTodosAlertRegularUserTest.java
@@ -1,45 +1,41 @@
 package com.qacart.tasky.testcases;
 
 import com.qacart.tasky.bases.BaseTest;
-import com.qacart.tasky.model.SubscriptionData;
 import com.qacart.tasky.model.UserLogin;
 import com.qacart.tasky.model.UserRegistration;
 import com.qacart.tasky.pages.DashboardPage;
 import com.qacart.tasky.pages.LoginPage;
 import com.qacart.tasky.pages.RegisterPage;
-import com.qacart.tasky.pages.SubscriptionPage;
+import com.qacart.tasky.pages.TodoPage;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.qacart.tasky.fixtures.UserFixture.getDefaultUserForLogin;
 import static com.qacart.tasky.fixtures.UserFixture.getDefaultUserForRegister;
-import static com.qacart.tasky.fixtures.subscriptionFixture.getSubscriptionData;
-import static com.qacart.tasky.mocker.MockCurrentSubscription.makeTheSubscriptionCloseToExpiration;
+import static com.qacart.tasky.mocker.TodosMocker.mockTodosForRegularUser;
 
-public class RenewalTest extends BaseTest {
+public class MaximumTodosAlertRegularUserTest extends BaseTest {
     private final RegisterPage registerPage = new RegisterPage();
     private final LoginPage loginPage = new LoginPage();
     private final DashboardPage dashboardPage = new DashboardPage();
-    private final SubscriptionPage subscriptionPage = new SubscriptionPage();
+    private final TodoPage todoPage = new TodoPage();
     @BeforeMethod
-    public void setUp() throws InterruptedException {
+    public void setUp() {
         registerPage.loadPage();
         UserRegistration userDataRegistration = getDefaultUserForRegister();
         registerPage.registerUsingAPI(userDataRegistration);
         loginPage.loadPage();
         UserLogin userForLogin = getDefaultUserForLogin();
-        String accessToken = loginPage.loginUsingAPI(userForLogin);
+        loginPage.loginUsingAPI(userForLogin);
         dashboardPage.loadPage();
-        dashboardPage.clickSubscriptionTab();
-        SubscriptionData subscriptionData = getSubscriptionData();
-        subscriptionPage.subscribeUsingAPI(subscriptionData,accessToken);
+        todoPage.loadPage();
     }
 
     @Test
-    void renewalMessageShouldBeDisplayedBeforeTwoDaysOfSubscriptionExpiration() throws InterruptedException {
-        makeTheSubscriptionCloseToExpiration();
-        dashboardPage.loadPage();
-        Assert.assertTrue(dashboardPage.isRenewButtonPresent());
+    public void MaximumAlertMessageShouldBeShownIfTheRegularUserHaveThreeTodos() {
+        mockTodosForRegularUser();
+        todoPage.loadPage();
+        Assert.assertTrue(todoPage.getLimiterMessage().contains("maximum number of todos"));
     }
 }
