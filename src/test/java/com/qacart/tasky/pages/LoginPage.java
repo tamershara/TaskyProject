f912c8c5e2762737928e33c8fb712c9138f43d1d
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 20:07:50 +0300

    Create login function using api and make it return the access token to use it in other apis that need it

--- src/test/java/com/qacart/tasky/pages/LoginPage.java
+++ src/test/java/com/qacart/tasky/pages/LoginPage.java
@@ -1,5 +1,10 @@
 package com.qacart.tasky.pages;
 
+import com.qacart.tasky.model.LoginResponse;
+import com.qacart.tasky.model.UserLogin;
+import io.restassured.response.Response;
+
+import static com.qacart.tasky.clients.UserClient.loginAPI;
 import static com.qacart.tasky.configs.ConfigFactory.getConfig;
 import static com.qacart.tasky.driver.managers.DriverManager.getDriver;
 
@@ -7,5 +12,11 @@ public class LoginPage implements BasePage {
     @Override
     public void loadPage() {
         getDriver().get(getConfig().pageBaseURL() + "/login");
+    }
+
+    public String loginUsingAPI(UserLogin user) {
+        Response loginResponse = loginAPI(user);
+        LoginResponse login = loginResponse.as(LoginResponse.class);
+        return login.getToken();
     }
 }
