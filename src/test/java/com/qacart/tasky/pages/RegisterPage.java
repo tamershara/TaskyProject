ee957755a86d58bbf5554f7669ee9cf728dc5c4b
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 20:10:24 +0300

    Create register function using api to use it to register user

--- src/test/java/com/qacart/tasky/pages/RegisterPage.java
+++ src/test/java/com/qacart/tasky/pages/RegisterPage.java
@@ -1,5 +1,8 @@
 package com.qacart.tasky.pages;
 
+import com.qacart.tasky.model.UserRegistration;
+
+import static com.qacart.tasky.clients.UserClient.registerAPI;
 import static com.qacart.tasky.configs.ConfigFactory.getConfig;
 import static com.qacart.tasky.driver.managers.DriverManager.getDriver;
 
@@ -7,5 +10,9 @@ public class RegisterPage implements BasePage {
     @Override
     public void loadPage() {
         getDriver().get(getConfig().pageBaseURL() + "/register");
+    }
+
+    public void registerUsingAPI(UserRegistration user) {
+        registerAPI(user);
     }
 }
