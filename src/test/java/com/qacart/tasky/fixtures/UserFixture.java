b023c54dcdb135528486f6ff90feda0fcca09a52
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 19:50:20 +0300

    Develop the creation of test data using model and fixture packages, and generate random email in util package

---
+++ src/test/java/com/qacart/tasky/fixtures/UserFixture.java
@@ -0,0 +1,29 @@
+package com.qacart.tasky.fixtures;
+
+import com.qacart.tasky.model.UserLogin;
+import com.qacart.tasky.model.UserRegistration;
+
+import static com.qacart.tasky.utilites.RandomDataUtil.getRandomEmail;
+
+public final class UserFixture {
+    private UserFixture() {}
+    private final static String email = getRandomEmail();
+    public static UserRegistration getDefaultUserForRegister() {
+        return UserRegistration
+                .builder()
+                .setEmail(email)
+                .setFirstName("test")
+                .setLastName("automation")
+                .setPassword("test12")
+                .setUserType("regular")
+                .build();
+    }
+
+    public static UserLogin getDefaultUserForLogin() {
+        return UserLogin
+                .builder()
+                .setEmail(email)
+                .setPassword("test12")
+                .build();
+    }
+}
