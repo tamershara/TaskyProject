b5cb94ec69381854de4918712b85eb84ce531d59
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-05 20:16:01 +0300

    Create function to click on subscription tab to redirect user there to let him subscribe

--- src/test/java/com/qacart/tasky/pages/DashboardPage.java
+++ src/test/java/com/qacart/tasky/pages/DashboardPage.java
@@ -1,11 +1,18 @@
 package com.qacart.tasky.pages;
 
+import org.openqa.selenium.By;
+
 import static com.qacart.tasky.configs.ConfigFactory.getConfig;
 import static com.qacart.tasky.driver.managers.DriverManager.getDriver;
 
 public class DashboardPage implements BasePage {
+    private final By subscriptionButtonLocator = By.cssSelector("[data-testid=SubscriptionsIcon]");
     @Override
     public void loadPage() {
         getDriver().get(getConfig().pageBaseURL() + "/dashboard");
+    }
+
+    public void clickSubscriptionButton() {
+        getDriver().findElement(subscriptionButtonLocator).click();
     }
 }
